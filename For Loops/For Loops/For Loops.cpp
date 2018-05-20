7a899ff9bfd192e1c1e2f1cbf332c47d119bf83e
SamGood2404 <38388332+SamGood2404@users.noreply.github.com>
2018-05-20 20:20:12 +1200

    For Loops
    SG

---
+++ For Loops/For Loops/For Loops.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	for (int i = 0; i < 10; i++)
+	{
+		cout << "Hello " << i << endl;
+		cout << endl;
+	}
+
+	int iTemp;
+	cin >> iTemp;
+
+	return(0);
+}
